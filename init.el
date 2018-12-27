9593381a849904b9c2b6f00b45428ed5bbd3b4d7
Ahmad Bakr <a.bakr@catawiki.nl>
2018-12-27 17:07:47 +0100

    remove unused layers

--- init.el
+++ init.el
@@ -33,15 +33,11 @@ values."
    '(
      csv
      ruby
-     ruby
-     go
-     sql
      typescript
      html
      markdown
      javascript
      ansible
-     ruby
      osx
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
@@ -62,10 +58,8 @@ values."
      ;; syntax-checking
      auto-completion
      version-control
-     ruby-on-rails
      search-engine
      github
-     themes-megapack
      fzf
      )
    ;; List of additional packages that will be installed without being
@@ -73,9 +67,7 @@ values."
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
    dotspacemacs-additional-packages '(
-    all-the-icons
     helm-git-grep
-    enh-ruby-mode
     ag
     dash-at-point
     multiple-cursors
@@ -153,8 +145,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(ample
-                         spacemacs-dark
+   dotspacemacs-themes '(spacemacs-dark
                          spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
@@ -336,11 +327,11 @@ This function is called at the very end of Spacemacs initialization after
 layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
-you should place your code here."
+qyou should place your code here."
 
  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
- (set-face-attribute 'default nil :height 134)
+ (set-face-attribute 'default nil :height 135)
 
  (global-set-key [f8] 'neotree-toggle)
  (global-set-key (kbd "C-c n") 'neotree-dir)
